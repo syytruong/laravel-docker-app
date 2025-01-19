3fcd0bf7c5409a4a1ed2435255a468e32420d176
syytruong <syytruong@gmail.com>
2025-01-19 22:11:16 +0700

    CHORE update orders table

--- database/migrations/2025_01_19_145901_create_orders_table.php
+++ database/migrations/2025_01_19_145901_create_orders_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('address');
+            $table->json('basket');
             $table->timestamps();
         });
     }
